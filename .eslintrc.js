6e8d94b55cec9126e79551f8f9439bd9ab9178cb
Slawomir CALUCH <scaluch@gmail.com>
2017-01-23 12:09:03 +0100

    project structure

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "eol-last": [
+            "error"
+        ],
+        "no-multiple-empty-lines": ["error", {"maxBOF": 0, "max": 3, "maxEOF": 0}],
+        "no-trailing-spaces": ["error"],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
